2f90786e534b2ec23f670c8d82aff04829029465
Anas Yousif <Anasyousif182@gmail.com>
2023-10-27 08:54:39 -0400

    today's lesson 1

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,11 +4,58 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+// bluit in functions
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("")
 }
 
+//ES6 version
+function reverseString(text) {
+    return [...text].reverse().join('')
+}
+
+// for loop
+function reverseString(text) {
+    let result = ""
+
+    for (let i = text.length -1; i >= 0; i--){
+    result += text[i]
+    }
+    return result
+}
+
+// for of used in ES6
+function reverseString(text) {
+    let result = "";
+
+    for(let char of text){
+        result = char + result
+    }
+
+    return result;
+}
+
+// recursion
+
+function reverseString(text) {
+    if (text === "") {
+        return ""
+    }else{
+        return reverseString(text.substr(1)) + text[0]
+    }
+    
+}
+
+// using reduce 
+
+function reverseString(text) {
+    return text.split("").reduce((acc,char) =>  char + acc, '')
+}
+
+// using ES6
+function reverseString(text) {
+    return [...text].reduce((acc, char) => char + acc , '')
+}
 
 
 module.exports = reverseString
